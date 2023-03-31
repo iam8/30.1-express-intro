6f934cbfeeb11a44f7070e4fc7899cf15ee3b79a
alexm <alex-m@outlook.com>
2023-03-30 18:24:53 -0700

    Import express and set up minimalist server (to test that it works so far)

---
+++ app.js
@@ -0,0 +1,16 @@
+// Ioana A Mititean
+// Exercise 30.1 - Express Routing/Calculator Exercise
+
+const express = require("express");
+const app = express();
+
+
+
+
+
+
+
+
+app.listen(3000, "127.0.0.1", () => {
+    console.log("App running on 127.0.0.1, port 3000");
+})
