04ebfe3df901b5d17860dfa0a783515f40abb25f
alexm <alex-m@outlook.com>
2023-03-30 19:04:28 -0700

    Add working code to route for mean operation

--- app.js
+++ app.js
@@ -6,7 +6,28 @@ const app = express();
 
 
 app.get("/mean", (req, res) => {
-    return res.send("You have reached the mean page");
+
+    const queryStr = req.query["nums"];
+
+    // Case: empty or missing query string
+    if (!queryStr) {
+        return res.json({
+            operation: "mean",
+            value: "None"
+        })
+    }
+
+    const nums = queryStr.split(",");
+
+    // Calculate sum of the nums
+    const sum = nums.reduce((accum, curr) => {
+        return accum + (+curr);
+    }, 0);
+
+    return res.json({
+        operation: "mean",
+        value: sum / nums.length
+    });
 })
 
 app.get("/median", (req, res) => {
