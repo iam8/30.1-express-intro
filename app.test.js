24690bba480f54c9c03904df052322a91ae1d469
alexm <alex-m@outlook.com>
2023-04-03 16:08:29 -0700

    Import helper functions from app file; set up describe() for each function to test

--- app.test.js
+++ app.test.js
@@ -3,3 +3,35 @@
 
 /** Unit tests for mean, median, and mode Express application. */
 
+const {
+    areAllNumbers,
+    calculateMean,
+    calculateMedian,
+    calculateMode,
+    validateQueryString
+} = require("./app");
+
+
+describe("Tests for areAllNumbers function", () => {
+
+})
+
+
+describe("Tests for calculateMean function", () => {
+
+})
+
+
+describe("Tests for calculateMedian function", () => {
+
+})
+
+
+describe("Tests for calculateMode function", () => {
+
+})
+
+
+describe("Tests for validateQueryString function", () => {
+
+})
