6fa49dd82d15ae3a93b660a9ba0c748c5447e785
alexm <alex-m@outlook.com>
2023-04-03 16:38:00 -0700

    Add code for remaining tests for areAllNumbers

--- app.test.js
+++ app.test.js
@@ -20,19 +20,23 @@ describe("Tests for areAllNumbers function", () => {
     })
 
     test("Should return true for all numerical inputs", () => {
-
+        const res = areAllNumbers([-3, -1.5, 0, 1.5, 3]);
+        expect(res).toEqual(true);
     })
 
     test("Should return true for number string inputs", () => {
-
+        const res = areAllNumbers(["-7", "-2.5", "0", "2.5", "7"]);
+        expect(res).toEqual(true);
     })
 
     test("Should return false for all non-numerical inputs", () => {
-
+        const res = areAllNumbers(["blah", "string", {}, [], true]);
+        expect(res).toEqual(false);
     })
 
     test("Should return false if only one input is non-numerical", () => {
-
+        const res = areAllNumbers([0, 1, 2, "psych"]);
+        expect(res).toEqual(false);
     })
 
 })
