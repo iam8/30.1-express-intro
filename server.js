ee8b434c3f2e2fe908606590d1b8008085c8d6aa
alexm <alex-m@outlook.com>
2023-04-03 16:30:32 -0700

    Create file that starts Express server for app

---
+++ server.js
@@ -0,0 +1,11 @@
+// Ioana A Mititean
+// Exercise 30.1 - Express Routing/Calculator Exercise
+
+/** Start Express server for app. */
+
+const { app } = require("./app");
+
+
+app.listen(3000, "127.0.0.1", () => {
+    console.log("App running on 127.0.0.1, port 3000");
+})
